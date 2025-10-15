a2def218ab79f43d5704dd0e50d9cfe52279d2f4
Md. Ariful Ahsan <supal.iut03@gmail.com>
2025-10-15 21:38:28 +0200

    unused code removal

--- task-2.py
+++ task-2.py
@@ -9,7 +9,6 @@ output_dir = "output"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-a = "Stockholm"
 # desktop user-agent
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
